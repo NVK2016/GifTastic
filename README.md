d00d564a31c23524890e223236bdc35d9e5dbd77
Namita Shenai <namita.shenai@gmail.com>
2019-03-05 19:39:25 -0800

    Added the logic for animation, rendering buttons & adding new buttons

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # GifTastic
 
 Using the GIPHY API to make a dynamic web page that populates with gifs of your choice.
-When the user selects any one of the diaplyed topic the site grabs 10 static,  non-animated gif images from the GIPHY API and place them on the page.
+When the user selects any one of the displayed topic the site grabs 10 static,  non-animated gif images from the GIPHY API and place them on the page.
 User clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
 ###Built With: 
@@ -9,10 +9,10 @@ User clicks one of the still GIPHY images, the gif should animate. If the user c
 * HTML - displays the website 
 * CSS & bootstrap - for cosmetic design of the site. 
 * Javascript & Jquery - hold the main app logic that would help gettting dynamic data from the web. 
-* AJAX - used to get a response for the give API Url. 
+* AJAX - used to get a response for the give API Url which grabs all the gifs related to the topic chosen or enterned.  
 
 ### Screenshots: 
-
+![alt txt](https://)
 
 
 ## Link to the Giftastic Site 
---
+++ assets/javascript/giftastic.js
@@ -0,0 +1,123 @@
+$(document).ready(function() {
+
+    // VARIABLES 
+    //--------------------------------------------------
+    var topics = [
+      "minions", "disney-cars","inside out", "wall-e", "disney-brave","la luna", "coco","cars3"
+    ];
+
+
+    // FUNCTIONS 
+    // -------------------------------------------
+      function renderButtons(){
+
+        console.log("renderButtons");
+        // Delete the content inside the gif-buttons div prior to adding new gif buttons 
+        $("#gif-buttons").empty(); 
+
+        // Loop through the array of topics, then generate buttons for each topic in the array
+        for(var i=0; i< topics.length; i++)
+        {
+          //Create button 
+          var gifButton = $("<button>");
+          gifButton.addClass("gifTastic");  //its helpful when we use event listeners 
+          //Add movie name 
+          gifButton.attr("data-name", topics[i]);  
+          gifButton.text(topics[i]);
+          console.log(gifButton);
+          $('#gif-buttons').append(gifButton);
+      }
+    }
+
+  // $(document).on("click", ".gifTastic", displayMovieInfo);
+  // $(".gifTastic").on("click", function() {
+  function displayGifInfo() {
+
+    //CLear previous GIF IMAGES 
+    $("#gifs-appear-here").empty();
+
+    //Take the GIF NAME AS A PARAMETER 
+    var gifTopic = $(this).attr("data-name");
+
+    //RETURNS 10 items for the selected topic
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + gifTopic + "&api_key=4aCPsmsCn0T77A2YUnVUvvAFYd6t9it8&limit=10";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function (response) {
+      var results = response.data;
+      // console.log(results);
+
+      for (var i = 0; i < results.length; i++) {
+        var newGifDiv = $("<div>");
+
+        var rating = results[i].rating;
+
+        var p = $("<p>").text("Rating: " + rating);
+
+        var gifImage = $("<img>");
+        //STILL & ANIMATE URLS 
+        var animateImgURL = results[i].images.fixed_height.url;
+        var stillImgUrl = results[i].images.fixed_height_still.url;
+        // console.log(animateImgURL); 
+        // console.log(stillImgUrl); 
+
+        gifImage.attr("src", stillImgUrl);
+        gifImage.attr("data-still", stillImgUrl);
+        gifImage.attr("data-animate", animateImgURL);
+        gifImage.attr("data-state", "still");
+        gifImage.addClass("gif-animation");
+
+        newGifDiv.prepend(p);
+        newGifDiv.prepend(gifImage);
+
+        $("#gifs-appear-here").prepend(newGifDiv);
+      }
+    });
+
+  }
+
+    //EVENT LISTINERS 
+    //-------------------------------------------
+   
+    // This function handles events where the add topic SUBMIT button is clicked
+    $("#add-gifs").on("click", function(event) {
+      event.preventDefault();
+      // This line of code will grab the input from the textbox
+      var topic = $("#topic-input").val().trim();
+
+      // The movie from the textbox is then added to our array
+      topics.push(topic);
+
+      // Calling renderButtons which handles the processing of our gif array
+      renderButtons();
+    });
+
+     //ON BUTTON CLICK ANIMATE OR STILL THE GIF IMAGES 
+     $(document).on("click", ".gif-animation", function() {
+      // console.log("Animation IMage click"); 
+      //GIF IMAGE STATE 
+      var state = $(this).attr("data-state"); 
+      // console.log(state);
+
+      if (state === "still") {
+        $(this).attr("src",  $(this).attr("data-animate")); 
+        $(this).attr("data-state", "animate"); 
+      }
+      else {
+        $(this).attr("src",  $(this).attr("data-still")); 
+        $(this).attr("data-state", "still"); 
+      }
+
+    });
+    
+    //MAIN PROCESS
+    // -----------------
+
+    // Calling the renderButtons function to display the intial buttons
+    renderButtons(); 
+    // Adding click event listeners to all elements with a class of "gifTastic"
+    $(document).on("click", ".gifTastic", displayGifInfo); 
+    
+});  
