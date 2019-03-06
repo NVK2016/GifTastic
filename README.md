adb481cbc57edd47fc52ff78858635b57df1fcd7
Namita Shenai <namita.shenai@gmail.com>
2019-03-06 00:01:43 -0800

    User can search from two API options

--- README.md
+++ README.md
@@ -4,10 +4,13 @@ Using the GIPHY API to make a dynamic web page that populates with gifs of your 
 When the user selects any one of the displayed topic the site grabs 10 static,  non-animated gif images from the GIPHY API and place them on the page.
 User clicks one of the still GIPHY images, the gif should animate. If the user clicks the gif again, it should stop playing.
 
+Highlights: 
+* user can even search  BAnds in Town API  by clicking the button 
+
 ###Built With: 
 * GIPHY API - to display all the animated gifs based on the selected topic. 
 * HTML - displays the website 
-* CSS & bootstrap - for cosmetic design of the site. 
+* CSS & bootstrap - for cosmetic design of the site and make it mobile responsive. 
 * Javascript & Jquery - hold the main app logic that would help gettting dynamic data from the web. 
 * AJAX - used to get a response for the give API Url which grabs all the gifs related to the topic chosen or enterned.  
 
--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -3,15 +3,23 @@ $(document).ready(function() {
     // VARIABLES 
     //--------------------------------------------------
     var topics = [
-      "minions", "disney-cars","inside out", "wall-e", "disney-brave","la luna", "coco","cars3"
+      "minions", "disney-cars","inside out", "wall-e", "disney-brave","la luna", "UP",
+      "snow white" , "disney-monsters", "disney-toy story"
     ];
 
+    // // get favorites from local storage or empty array
+    // var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+
+    // // add class 'fav' to each favorite
+    // favorites.forEach(function(favorite) {
+    //   document.getElementById(favorite).className = 'fav';
+    // });
 
     // FUNCTIONS 
     // -------------------------------------------
       function renderButtons(){
 
-        console.log("renderButtons");
+        // console.log("renderButtons");
         // Delete the content inside the gif-buttons div prior to adding new gif buttons 
         $("#gif-buttons").empty(); 
 
@@ -24,7 +32,7 @@ $(document).ready(function() {
           //Add movie name 
           gifButton.attr("data-name", topics[i]);  
           gifButton.text(topics[i]);
-          console.log(gifButton);
+
           $('#gif-buttons').append(gifButton);
       }
     }
@@ -34,7 +42,7 @@ $(document).ready(function() {
   function displayGifInfo() {
 
     //CLear previous GIF IMAGES 
-    // $("#gifs-appear-here").empty();
+    $("#gifs-appear-here").empty();
 
     //Take the GIF NAME AS A PARAMETER 
     var gifTopic = $(this).attr("data-name");
@@ -50,11 +58,16 @@ $(document).ready(function() {
       console.log(results);
 
       for (var i = 0; i < results.length; i++) {
-        var newGifDiv = $("<div id='gif-"+ i +"'>");
+        var newGifDiv = $("<div id='gif-"+ i +"' class='col-md-6' style='float:left;'>");
 
         var rating = results[i].rating;
 
+        //Rating condition 
+        if (!( rating === "pg-13" || rating === "r" || rating === "gv" )){
+
         var p = $("<p>").text("Rating: " + rating);
+        p.append("<br/> Title: " + results[i].title);
+        p.append("<br/> Click the gif to animate it!!");
 
         var gifImage = $("<img>");
         //STILL & ANIMATE URLS 
@@ -69,10 +82,11 @@ $(document).ready(function() {
         gifImage.attr("data-state", "still");
         gifImage.addClass("gif-animation");
 
-        newGifDiv.prepend(p);
-        newGifDiv.prepend(gifImage);
+        newGifDiv.append(p);
+        newGifDiv.append(gifImage);
 
-        $("#gifs-appear-here").prepend(newGifDiv);
+        $("#gifs-appear-here").append(newGifDiv);
+        }
       }
     });
 
@@ -111,9 +125,96 @@ $(document).ready(function() {
       }
 
     });
+
+    // BANDS in TOWN API 
+    //----------------------------------------------------
+
+    function searchBandsInTown(artist) {
+      console.log("searchBandsInTown");
+      // Add code to query the bands in town api searching for the artist received as an argument to this function
+       // This is our API key. Add your own API key between the ""
+       $("#artist-div").empty();
+
+      // Here we are building the URL we need to query the database
+      var queryURL = "https://rest.bandsintown.com/artists/"+artist.split(' ').join('%20')+"?app_id=54beed58-86b2-4068-bf0e-d05667919404";
+  
+      // We then created an AJAX call
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      }).then(function(response) {
+  
+        // Create CODE HERE to Log the queryURL
+        // console.log(queryURL)
+        // Create CODE HERE to log the resulting object
+        // console.log(response)
+       
+            // Using jQuery, append the following to the #artist-div :
+            // The artist's name
+            $("#artist-div").append("<h5> Artist Name: " + response.name + "</h5>"); 
+            // The artists thumbnail image
+            var newImg = $("<br /> <img class='gif-animation' src="+ response.thumb_url+" /> <br/>"); 
+            $("#artist-div").append(newImg); 
+            // The number of fans tracking this artist
+            $("#artist-div").append("<p class='txt-light'> Number of Fans: " + response.tracker_count + "</p>"); 
+            // The number of upcoming events for this artist
+            $("#artist-div").append("<p class='txt-light'> Upcoming Events: " + response.upcoming_event_count+ "</p>"); 
+            // A link to the bandsintown url for this artist
+            // Note: Append actual HTML elements, not just text
+            $("#artist-div").append("<a id='artist-link' href='"+response.url+"' > Click HERE to track events </a>"); 
+          });
+    }
+  
+    // Event handler for user clicking the select-artist button
+    $("#select-artist").on("click", function(event) {
+      // Preventing the button from trying to submit the form
+      event.preventDefault();
+      // Storing the artist name
+      var artist = $("#artist-input").val().trim();
+      console.log(artist);
+      if ( artist !== null && artist !== ""){
+      // Running the searchBandsInTown function (passing in the artist as an argument)
+      searchBandsInTown(artist);
+      }
+      else {
+        alert("Please enter an artist name"); 
+        $("#artist-input").focus(); 
+      }
+    });
     
     //MAIN PROCESS
     // -----------------
+    $("#pixarDisplay").hide(); 
+    $("#bandDisplay").hide(); 
+
+      //On click of start button the game starts 
+    $('#btn-pixar').click(function () {
+      console.log("Pixar Button clicked");
+
+      $('#btn-pixar').hide();
+      $('#btn-artist').show();
+
+      $("#pixarDisplay").show(); 
+      $("#bandDisplay").hide(); 
+
+      $("#gifs-appear-here").empty(); 
+      $("#artist-div").empty(); 
+    });
+
+    //On click of start button the game starts 
+    $('#btn-artist').click(function () {
+      console.log("Artist Band Button clicked");
+
+      $('#btn-pixar').show();
+      $('#btn-artist').hide(); 
+
+      $("#pixarDisplay").hide(); 
+      $("#bandDisplay").show(); 
+
+      $("#gifs-appear-here").empty(); 
+      $("#artist-div").empty(); 
+
+    });
 
     // Calling the renderButtons function to display the intial buttons
     renderButtons(); 
